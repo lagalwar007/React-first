d1aca91cf78ff44f7942c62bb907350c84984c76
Karan <lagalwar007@gmail.com>
2018-08-07 18:30:15 +0530

    material ui
    Install material ui

---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header, Footer } from "./layouts";
+import "../styles.css";
+
+export default props => (
+  <div className="App">
+    <Header />
+    <Footer />
+  </div>
+);
---
+++ src/Components/layouts/header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AppBar } from "@material-ui/core";
+
+export default props => (
+  <AppBar variant="condensed" color="secondary">
+    <h1>Header</h1>
+  </AppBar>
+);
